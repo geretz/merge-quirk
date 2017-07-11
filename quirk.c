73f80fce9486c856e2e5fb8a25328826f7b16396
Chaim Geretz <chaim.geretz@idt.net>
2017-07-11 15:54:00 -0400

    Common ancestor

---
+++ quirk.c
@@ -0,0 +1,14 @@
+
+	// a few comment lines
+	// that existed 
+	// in the common ancestor
+	// that get changed
+	if(1) {
+			if (f(1, 2))
+			{
+				if (thisLineMightDisapper(42)) {
+					t = time(0);
+				}
+			}
+	}
+}
