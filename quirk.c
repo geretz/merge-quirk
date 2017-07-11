52f1083bba48b65c072f78509e31d4f24be8b341
Chaim Geretz <chaim.geretz@idt.net>
2017-07-11 15:57:53 -0400

    Added comment line and code

--- quirk.c
+++ quirk.c
@@ -2,6 +2,7 @@
 	// a few comment lines
 	// that existed 
 	// in the common ancestor
+	// added this line in merge-src branch
 	// that get changed
 	if(1) {
 			if (f(1, 2))
@@ -10,5 +11,11 @@
 					t = time(0);
 				}
 			}
+	}
+
+	if (anotherFunction(1,2))
+	{
+		t = time(0)
+		f(0);
 	}
 }
